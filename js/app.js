a34fa4e229710e9d765e9e0404a04388900af769
Steven Le <lesteven1983@gmail.com>
2014-10-08 16:35:06 -0500

    portfolio
    progress

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-
-	
-
-
-}); 
+    
+    // function for smooth scrolling
+    $(document).on('click', 'nav li a', function(e) {
+    	
+        e.preventDefault();
+        
+        if($(this).attr("href")) {
+			var page = $(this).attr("href");
+		}
+		else {
+			var page = $(this).closest("a").attr("href");
+		}
+        
+        $("html, body").animate({scrollTop: parseInt($(page).position().top)}, 1000);
+        
+    });     
+    
+});
