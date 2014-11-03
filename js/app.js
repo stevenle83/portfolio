4987cdf18024b30146180e0d57782da096adb71a
Steven Le <lesteven1983@gmail.com>
2014-11-03 14:17:00 -0600

    portfolio
    finished project

--- js/app.js
+++ js/app.js
@@ -1,33 +1,20 @@
 $(document).ready(function() {
-    
     // function for smooth scrolling
     $(document).on('click', 'nav li a', function(e) {
-    	
         e.preventDefault();
-        
         if($(this).attr("href")) {
-			var page = $(this).attr("href");
-		}
-		else {
-			var page = $(this).closest("a").attr("href");
-		}
-        
+            var page = $(this).attr("href");
+        }
+        else {
+            var page = $(this).closest("a").attr("href");
+        }
         $("html, body").animate({scrollTop: parseInt($(page).position().top)}, 1000);
-        
     }); 
-
     //hover fadeIn/fadeOut effect for slide description
     $('.item').mouseenter(function() {
-
         $('.caption').fadeIn("slow");
-
             $(this).mouseleave(function() {
-
                 $('.caption').fadeOut("slow");
-
             });
-
     });  
-
-    
 });
